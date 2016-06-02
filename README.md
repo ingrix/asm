b2e415023e14606b0ac48045d1058ca94d4d5a1f
ingrix <ingrix@users.noreply.github.com>
2016-06-02 11:26:34 -0500

    Updated readme and cleaned up second hello world

--- README.md
+++ README.md
@@ -4,5 +4,8 @@ These are programs that I have written while teaching myself assembly language.
 
 ## Resources
 
+http://savannah.nongnu.org/projects/pgubook/ - Programming from the Ground Up, a good book on assembly programming in linux
+http://docs.cs.up.ac.za/programming/asm/derick_tut/ - another tutorial on assembly programming in linux
 https://www.cs.uaf.edu/courses/cs301/2014-fall/notes/pointer-arithmetic/ - outlines pointer arithmetic in C and assembly, useful for understanding how assembly handles pointers/values
-
+http://www.dreamincode.net/forums/topic/285550-nasm-linux-getting-command-line-parameters/ - working with command line parameters in assembly
+http://unixwiz.net/techtips/x86-jumps.html - quick reference for x86 jump instructions
--- hello_world_2/hello_world.asm
+++ hello_world_2/hello_world.asm
@@ -1,4 +1,5 @@
 ; Written for assembly with NASM
+; this program was written for x86-64 assembly on a linux system
 section .data
   hello: db 'Hello, world!',10 ; string to print
   hellolen: equ $-hello ; length of 'hello' line
@@ -7,33 +8,38 @@ section .text
   global _start
 
 _start:
-  call _stack
-  push hellolen
-  push hello
-  call _print
-  add rsp,16
+; we will use right-to-left parameter pushing
+; the _print call uses the write (2) system call so check the manpage for more info
+  push hellolen ; push buffer length onto stack
+  push hello ; push buffer address onto stack
+  call _print ; call the print function
+
+; pop rax
+; pop rax
+  add rsp,16 ; get rid of the parameters we passed to _print
+
   mov rax,0x01 ; syscall for exit()
   mov rbx,0x00 ; exit status 0
   int 0x80 ; software interrupt
 
-_stack:
-  mov rax,rsp
-  add rsp,8
-  sub rsp,8
-  ret
+_print: ; print a buffer to stdout, cdecl calling convention
 
-_print:
+; set up stack frame just because
   push rbp ; keep track of base pointer
   mov rbp,rsp ; move stack pointer to base now
-; at this point the stack should look like
-; +24 buffer length
-; +16 buffer pointer
-; +8 return address
-; 0 old ebp/current esp
+
+; at this point the stack should look like:
+; rsp+24 buffer length
+; rsp+16 buffer pointer
+; rsp+8 return instruction address 
+; rsp+0 old ebp/current esp
+
   mov rax,0x04 ; write()
-  mov rbx,0x01 ; stdout
-  mov rcx,[rbp+16]
-  mov rdx,[rbp+24]
+  mov rbx,0x01 ; stdout, arg 1 of write (2)
+  mov rcx,[rbp+16] ; buffer to write, arg 2
+  mov rdx,[rbp+24] ; buffer length, arg 3
   int 0x80 ; execute syscall
-  pop rbp
-  ret ; caller should clean up stack
+
+; remove rbp that we pushed so that rsp will point to the return address
+  pop rbp ; 
+  ret ; return to calling function, caller should clean up stack
